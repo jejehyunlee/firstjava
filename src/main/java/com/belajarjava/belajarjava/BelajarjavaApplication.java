58b7b9a9277db4df871bbeb4c21ab1e59db838f9
jejehyunlee <jefrisaputra989@gmail.com>
2022-06-05 10:57:51 +0700

    Fist Using Java

---
+++ src/main/java/com/belajarjava/belajarjava/BelajarjavaApplication.java
@@ -0,0 +1,13 @@
+package com.belajarjava.belajarjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarjavaApplication.class, args);
+	}
+
+}
